412113d86507d927b3a03671ea03c5ac3547d62c
Alastair Tse <alastair@liquidx.net>
2022-11-07 09:13:42 +0900

    Checked in geojson data is now moved off repo

--- src/routes/map/[slug]/+page.js
+++ src/routes/map/[slug]/+page.js
@@ -9,13 +9,13 @@ export async function load({ fetch, params }) {
   switch (params.slug) {
     case 'points': {
       const stations = await fetch('/tokyo-stations.csv').then(res => parseTokyoStations(res))
-      const outline = await fetch('/tokyo-mainland.geojson').then((res) => res.json())
-      const areas = await fetch('/tokyo-areas.geojson').then(res => parseTokyoAreas(res))
+      const outline = await fetch('/data/tokyo-mainland.geojson').then((res) => res.json())
+      const areas = await fetch('/data/tokyo-areas.geojson').then(res => parseTokyoAreas(res))
       const areasByWard = groupAreasByWard(outline, areas)
       return { stations, outline, areas, areasByWard }
     }
     case 'lines': {
-      const japanBoundingBox = await fetch('/japan-boundingbox.geojson').then((res) => res.json())
+      const japanBoundingBox = await fetch('/data/japan-boundingbox.geojson').then((res) => res.json())
       const trainLineNames = await fetch('/data/train-lines/index.json').then((res) => res.json())
       const trainLines = {}
       for (let name of trainLineNames) {
@@ -24,14 +24,15 @@ export async function load({ fetch, params }) {
       return { japanBoundingBox, trainLines }
     }
     case 'polygons': {
-      const outline = await fetch('/tokyo-mainland.geojson').then((res) => res.json())
-      const areas = await fetch('/tokyo-areas.geojson').then(res => parseTokyoAreas(res))
+      const outline = await fetch('/data/tokyo-mainland.geojson').then((res) => res.json())
+      const areas = await fetch('/data/tokyo-areas.geojson').then(res => parseTokyoAreas(res))
       return { outline, areas }
     }
     case 'green': {
-      const greenAreas = await fetch('/tokyo-green.geojson').then((res) => res.json())
-      const outline = await fetch('/tokyo-mainland.geojson').then((res) => res.json())
-      return { outline, areas: greenAreas }
+      const greenAreas = await fetch('/data/tokyo-green.geojson').then((res) => res.json())
+      const moreGreenArea = await fetch('/data/A45-19_13.geojson').then((res) => res.json())
+      const outline = await fetch('/data/tokyo-mainland.geojson').then((res) => res.json())
+      return { outline, areas: moreGreenArea }
 
     }
   }
